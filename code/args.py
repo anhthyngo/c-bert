497bae7e29e24c2b837eb9f80c341f1626dc4ceb
wh629 <60375376+wh629@users.noreply.github.com>
2020-04-11 17:44:40 -0400

    changed dataloading to match huggingface run_squad example

--- code/args.py
+++ code/args.py
@@ -7,12 +7,11 @@ import logging as log
 
 args = argparse.ArgumentParser(description='Continual BERT')
 
-args.add_argument('--data', type=str, default='/data',
-                  help='directory storing all data')
+
+
+
 args.add_argument('--save_dir', type=str, default='/results',
                     help='directory to save results')
-args.add_argument('--meta_epochs', type=int, default=100,
-                  help='number of epochs for meta-learning')
 args.add_argument('--meta_epochs', type=int, default=100,
                   help='number of epochs for meta-learning')
 args.add_argument('--fine_tune_epochs', type=int, default=100000,
@@ -27,3 +26,20 @@ args.add_argument('--seed', type=int, default=1111,
                     help='random seed')
 args.add_argument('--model', type=str, default='BERT',
                     help='name of RLN network. default is BERT')
+
+# used in huggingface run_squad example
+args.add_argument('--local_rank', ) # FINISH
+args.add_argument('--overwrite_cache', ) # FINISH
+args.add_argument('--predict_file', ) # FINISH
+args.add_argument('--train_file', ) # FINISH
+args.add_argument('--version_2_with_negative', type=bool, default=False,
+                  help='whether negative examples exist like in SQuADv2')
+args.add_argument('--max_seq_length', ) # FINISH
+args.add_argument('--doc_stride', ) # FINISH
+args.add_argument('--max_query_length', ) # FINISH
+args.add_argument('--threads', ) # FINISH
+args.add_argument('--local_rank', type=int, default=-1) # FINISH
+
+
+args.add_argument('--data_dir', type=str, default='data',
+                  help='directory storing all data')

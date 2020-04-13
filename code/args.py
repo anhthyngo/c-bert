baacd18621535d461142a0dc514d0a4b31c93218
wh629 <60375376+wh629@users.noreply.github.com>
2020-04-13 16:45:39 -0400

    added arguments and implemented directory checking

--- code/args.py
+++ code/args.py
@@ -1,18 +1,20 @@
 """
 Module for argument parcer.
 
-Many of the arguments are similar to Huggingface's run_squad example:
+Many of the arguments are from Huggingface's run_squad example:
 https://github.com/huggingface/transformers/blob/7972a4019f4bc9f85fd358f42249b90f9cd27c68/examples/run_squad.py
 """
 import argparse
 import os
-import logging as log
 
 args = argparse.ArgumentParser(description='Continual BERT')
 
 
 
-
+args.add_argument('--experiment',
+                  type=str,
+                  default='testing',
+                  help='name of experiment')
 args.add_argument('--save_dir',
                   type=str, 
                   default='results',
@@ -21,9 +23,18 @@ args.add_argument('--seed',
                   type=int,
                   default=1111,
                   help='random seed')
-args.add_argument('--verbose_log',
-                  action='store_true',
-                  help='whether to store verbose logging')
+args.add_argument('--run_log',
+                  type=str,
+                  default=os.join.path(os.getcwd(),'log'),
+                  help='where to print run log')
+args.add_argument('--tasks',
+                  type=str,
+                  default='SQuAD,TriviaQA-web,NewsQA,SearchQA,HotpotQA,NaturalQuestionsShort',
+                  help='all the tasks for the experiment, separated by ,')
+args.add_argument('--continual_curriculum',
+                  type=str,
+                  default='SQuAD,TriviaQA-web',
+                  help='tasks in order for continual learning, separated by ,')
 
 # =============================================================================
 # for model definition
@@ -47,9 +58,6 @@ args.add_argument('--local_rank',
 args.add_argument('--overwrite_cache', 
                   action='store_true',
                   help='overwrite the cached data sets')
-args.add_argument('--version_2_with_negative',
-                  action='store_true',
-                  help='whether negative examples exist like in SQuADv2')
 args.add_argument('--max_seq_length',
                   type=int,
                   default=384,
@@ -78,8 +86,8 @@ args.add_argument('--data_dir',
                   help='directory storing all data')
 args.add_argument('--batch_size', 
                   type=int, 
-                  default=20,
-                   help='batch size')
+                  default=32,
+                  help='batch size')
 
 # =============================================================================
 # for training
@@ -87,17 +95,17 @@ args.add_argument('--batch_size',
 args.add_argument('--fp16',
                   action='store_true',
                   help='whether to use 16-bit precision (through NVIDIA apex)')
-args.add_argument('--meta_epochs',
+args.add_argument('--meta_steps',
                   type=int,
                   default=100,
-                  help='number of epochs for meta-learning')
-args.add_argument('--fine_tune_epochs', 
+                  help='number of updates for meta-learning')
+args.add_argument('--fine_tune_steps', 
                   type=int, 
                   default=100000,
-                  help='number of epochs for fine-tuning')
+                  help='number of updates for fine-tuning')
 args.add_argument('--learning_rate', 
                   type=float, 
-                  default=5e-5,
+                  default=5e-3,
                   help='initial learning rate for Adam')
 args.add_argument("--weight_decay",
                   type=float,
@@ -122,7 +130,7 @@ args.add_argument('--n_best_size',
 args.add_argument('--max_answer_length',
                   type=int,
                   default=30,
-                  help='max lenght of answer. needed because start'
+                  help='max length of answer. needed because start'
                   'and end not conditioned on eachother')
 args.add_argument('--logging_steps',
                   type=int,
@@ -132,4 +140,28 @@ args.add_argument('--save_steps',
                   type=int,
                   default=500,
                   help='Save best weights every X update steps')
+args.add_argument('--verbose_steps',
+                  type=int,
+                  default=1000,
+                  help='Log results ever X update steps')
+args.add_argument('--verbose_log',
+                  action='store_true',
+                  help='whether to store verbose logging in evaluation')
+args.add_argument("--null_score_diff_threshold",
+                  type=float,
+                  default=0.0,
+                  help="If null_score - best_non_null is greater than the threshold predict null.")
+args.add_argument("--do_lower_case",
+                  action="store_true",
+                  help="Set this flag if you are using an uncased model.")
+args.add_argument('--version_2_with_negative',
+                  action='store_true',
+                  help='whether negative examples exist like in SQuADv2')
 
+def check_paths(parser):
+    """
+    make sure directories exist
+    """
+    assert os.path.exists(parser.data_dir), "Data directory does not exist"
+    assert os.path.exists(parser.save_dir), "Save directory does not exist"
+    assert os.path.exists(parser.run_log),  "Run logging directory does not exist"

69507abf3c16618c5723a0d51b6c64add87ec6f4
wh629 <60375376+wh629@users.noreply.github.com>
2020-04-13 14:29:35 -0400

    deleted some unnecessary things

--- code/model.py
+++ code/model.py
@@ -1,45 +1,49 @@
 """
-    Module to define our model (RLN, PLN)
+Module to define our model (RLN, PLN)
 
-    BERT-M -> SQuAD -> TriviaQA
-    BERT -> SQuAD -> TriviaQA
+BERT-M -> SQuAD -> TriviaQA
+BERT -> SQuAD -> TriviaQA
 
-    ref code:
-    https://colab.research.google.com/drive/1pTuQhug6Dhl9XalKB0zUGf4FIdYFlpcX#scrollTo=1sjzRT1V0zwm
-    https://github.com/huggingface/transformers/blob/master/src/transformers/modeling_bert.py
+ref code:
+https://colab.research.google.com/drive/1pTuQhug6Dhl9XalKB0zUGf4FIdYFlpcX#scrollTo=1sjzRT1V0zwm
+https://github.com/huggingface/transformers/blob/master/src/transformers/modeling_bert.py
 
 """
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from transformers import BertModel, BertConfig
-import logging
-import tensorflow as tf
-
-import meta_learning
+import transformers
+import os
 
 
-# ============================ Set up Trainer model ============================
+# ============================ Set up a QA Model from Huggingface ============================
 
-class train_model(nn.Module):
-    """
+class QAModel(nn.Module):
+    def __init__(self,
+                 config,              # Huggingface configuration object for model
+                 load_rln = False,    # bool whether to load preloaded rln
+                 rln_weights = None   # name of state dict with rln weights
+                 ):
+        
+        """
         Define the architecture of the model
 
         set config = BertConfig.from_pretrained('bert-base-uncased')
 
         config = transformers.AutoConfig.from_pretrained(rep_name)
-    """
-    def __init__(self, config, meta, meta_weights = None):
-        super(self).__init__() #take transformers.BertConfig
+        """
+        
+        super(self).__init__()
         self.num_labels = config.num_labels
 
-        # === load weights for BERT or meta-BERT ===
+        # Define model
         self.model = transformers.AutoModelForQuestionAnswering.from_config(config)
-        # only supporting BERT
-        if meta:
-            self.model.bert.load_state_dict(torch.load(meta_weights))
-
+        
+        # load meta learned weights 
+        # only supporting BERT for now
+        if load_rln:
+            assert os.path.exist(rln_weights), "Path for meta-learned weights does not exist"
+            self.model.bert.load_state_dict(torch.load(rln_weights))
 
     def forward(
         self,
@@ -52,16 +56,22 @@ class train_model(nn.Module):
         start_positions = None,
         end_positions = None,
     ):
+        """
+        Forward used to feed Huggingface model.
+        
+        Returns output as specified below
+
+        """
         outputs = self.model(
-        input_ids = input_ids,
-        attention_mask = attention_mask,
-        token_type_ids = token_type_ids,
-        position_ids = position_ids,
-        head_mask = head_mask,
-        inputs_embeds = inputs_embeds,
-        start_positions = start_positions,
-        end_positions = end_positions
-        )
+            input_ids = input_ids,
+            attention_mask = attention_mask,
+            token_type_ids = token_type_ids,
+            position_ids = position_ids,
+            head_mask = head_mask,
+            inputs_embeds = inputs_embeds,
+            start_positions = start_positions,
+            end_positions = end_positions
+            )
     
         return outputs  # (loss), start_logits, end_logits, (hidden_states), (attentions)
 
@@ -73,16 +83,19 @@ class train_model(nn.Module):
         print('The BERT model has {:} different named parameters.\n'.format(len(params)))
 
         print('==== Embedding Layer ====\n')
-
+        
+        # might need to base indexing off self.model.config
         for p in params[0:5]:
             print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
 
         print('\n==== First Transformer ====\n')
-
+        
+        # might need to base indexing off self.model.config
         for p in params[5:21]:
             print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
 
         print('\n==== Output Layer ====\n')
-
+        
+        # might need to base indexing off self.model.config
         for p in params[-4:]:
             print("{:<55} {:>12}".format(p[0], str(tuple(p[1].size()))))
